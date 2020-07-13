d55759164b178df648c95238643dca54b660f0c9
Arturo Cullinane <artocully@users.noreply.github.com>
2020-07-13 15:42:10 +0100

    Add initial on change file upload function

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   console.log('document loaded');
+
+  const element = document.getElementById('theFile');
+  element.addEventListener('change', upload);
 });
+
+function upload() {
+  console.log('test');  
+  return new Promise(async (resolve, reject) => {
+    const filePicker = document.querySelector('input');
+
+    if (!filePicker
+        || !filePicker.files 
+        || filePicker.files.length <= 0
+      ) {
+        reject('No file selected.');
+        return;
+    }
+    
+    const myFile = filePicker.files[0];
+    
+    console.log(myFile);
+    
+    resolve();
+  }); 
+}
